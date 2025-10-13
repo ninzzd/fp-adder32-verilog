04ebee6dbff3a828613b91cac81683808a359a31
ninzzd <ninaad.desai1212@gmail.com>
2025-10-13 18:34:22 +0530

    Rename shiftr.v -> sr24.v Update fadd32.v : Larger mantissa obtained, shifting logic remains

--- src/fadd32.v
+++ src/fadd32.v
@@ -5,17 +5,26 @@ module fadd32(
     input [31:0] b,
     output [31:0] res
 );
-    wire sa;
-    wire sb;
-    wire [7:0] expa;
-    wire [7:0] expb;
-    wire [22:0] mana;
-    wire [22:0] manb;
-
-    assign sa = a[31];
-    assign sb = b[31];
-    assign expa = a[30:23];
-    assign expb = b[30:23];
-    assign mana = a[22:0];
-    assign manb = b[22:0];
+    wire a_isnorm;
+    wire b_isnorm;
+    wire exp_altb;
+    wire exp_aeqb;
+    wire exp_agtb;
+    wire [7:0] exp_absdiff;
+    comp8 exp_comp(
+        .a(a[30:23]),
+        .b(b[30:23]),
+        .a_eq_b(exp_aeqb),
+        .a_lt_b(exp_altb),
+        .a_gt_b(exp_agtb),
+        .abs_diff(exp_absdiff)
+    );
+    wire [23:0] man1; // mantissa of the number with larger exponent
+    wire [23:0] man2; // mantissa of the number with smaller exponent
+    assign a_isnorm = a[30]|a[29]|a[28]|a[27]|a[26]|a[25]|a[24]|a[23]; // 0 - a is subnormal, 1 - a is normal
+    assign b_isnorm = b[30]|b[29]|b[28]|b[27]|b[26]|b[25]|b[24]|b[23]; // 0 - b is subnormal, 1 - b is normal
+    assign man1[22:0] = {23{exp_agtb}}&a[22:0] | {23{~exp_agtb}}&b[22:0];
+    assign man1[23] = (exp_agtb)&(a_isnorm) | (~exp_agtb)&(b_isnorm);
+    assign man2[22:0] = {23{exp_agtb}}&b[22:0] | {23{~exp_agtb}}&a[22:0];
+    assign man2[23] = (exp_agtb)&(b_isnorm) | (~exp_agtb)&(a_isnorm);
 endmodule
---
+++ src/srl24.v
@@ -0,0 +1,8 @@
+// Author: Ninaad Desai
+// Description: 24-bit barrel logical right shifter with even rounding-off  
+module sr24(
+    input [23:0] oprnd,
+    input [4:0] shamt,
+    output [23:0] reslt
+);
+endmodule
