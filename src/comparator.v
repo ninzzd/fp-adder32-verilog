b096a8abf7046353538f40f13ea818ba95120100
ninzzd <ninaad.desai1212@gmail.com>
2025-10-13 16:10:42 +0530

    Create add8.v : Copied from RISC-V repo Create comparator.v : Uses add8 to compare 8-bit operands and output absolute difference, for exponent comparison and shifting

---
+++ src/comparator.v
@@ -0,0 +1,31 @@
+`timescale 1 ns / 1 ps
+module exp_comparator #(parameter T = 0.000)(
+    input [7:0] a,
+    input [7:0] b,
+    output a_lt_b,
+    output a_eq_b,
+    output a_gt_b,
+    output [7:0] abs_diff
+);
+    wire cout;
+    wire ne;
+    wire [7:0] s;
+    add8 adder1(
+        .a(a),
+        .b(~b),
+        .c_1(1'b1),
+        .c7(cout),
+        .s(s)
+    );
+    assign ne = s[7]|s[6]|s[5]|s[4]|s[3]|s[2]|s[1]|s[0];
+    assign a_eq_b = ~ne;
+    assign a_gt_b = ne&cout;
+    assign a_lt_b = ne&~cout;
+    add8 adder2(
+        .a(s^{8{a_lt_b}}),
+        .b(8'b00000000),
+        .c_1(a_lt_b),
+        .s(abs_diff)
+    );
+
+endmodule
---
+++ tb/comparator_tb.v
@@ -0,0 +1,4 @@
+module comparator_tb;
+    reg [7:0] a;
+    reg [7:0] b;
+endmodule
