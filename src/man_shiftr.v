e9a74558a905d4564f7d8f74727b9ba6f8ce0355
ninzzd <ninaad.desai1212@gmail.com>
2025-10-14 13:41:00 +0530

    Update man_shiftr.v : Shifter with G,R and S bits implemented, must be tested

--- src/man_shiftr.v
+++ src/man_shiftr.v
@@ -5,21 +5,46 @@ module man_shiftr(
     input [4:0] shamt,
     output [26:0] res
 );
-    wire [26:0] man_grs;
+    wire [25:0] man_gr;
     wire [25:0] smask;
+    wire [25:0] and1;
+    wire [25:0] or1;
     genvar i;
+
+    assign man_gr = {man,2'b00};
+
     generate
-        for(i = 1;i <= 26;i=i+1)
+        for(i = 0;i <= 25;i=i+1)
         begin
             mux32 m(
-                .in({{(5+i){1'b0}},man_grs[26:i]}),
+                .in({{(5+i){1'b0}},man_gr[26:i]}),
                 .sel(shamt),
-                .out(reslt[i])
+                .out(reslt[i+1])
             );
         end
-        for(i = 1;i <= 26;i=i+1)
+        for(i = 0;i <= 25;i=i+1) // Behavioral smask generation
         begin
-            
+            if(i < shamt)
+            begin
+                assign smask[i] = 1'b1;
+            end
+            else
+            begin
+                assign smask[i] = 1'b0;
+            end
+        end
+        for(i = 0;i <= 25;i=i+1)
+        begin
+            if(i == 0)
+            begin
+                assign or1[i] = and1[i];
+            end
+            else
+            begin
+                assign or1[i] = or1[i-1] | and1[i];
+            end
         end
     endgenerate
+    assign and1 = man_gr&smask;
+    assign res[0] = or1[25];
 endmodule
